28b246e1ad225fb79e8a23d1659dcac6a54c08e8
Chris Tauber <chrisjroeder252@hotmail.de>
2023-01-11 12:08:02 +0100

    feat: show permission request after opening the app

--- app/src/main/java/de/uniks/codecamp/group_a/weather/MainActivity.kt
+++ app/src/main/java/de/uniks/codecamp/group_a/weather/MainActivity.kt
@@ -1,8 +1,11 @@
 package de.uniks.codecamp.group_a.weather
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +16,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import de.uniks.codecamp.group_a.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // request permission to access the location, load the weather information
+        locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            // TODO: load the weather information from the viewmodel
+        }
+        locationPermissionRequest.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+
         setContent {
             WeatherTheme {
                 // A surface container using the 'background' color from the theme
