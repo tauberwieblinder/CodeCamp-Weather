9f705862d92fb1ae7a1f9e5f59aaf08b7b0a7f9d
Chris Tauber <chrisjroeder252@hotmail.de>
2023-01-13 11:09:42 +0100

    feat: bind implementations to interfaces (dependency injection)

---
+++ app/src/main/java/de/uniks/codecamp/group_a/weather/injection/AppModule.kt
@@ -0,0 +1,34 @@
+package de.uniks.codecamp.group_a.weather.injection
+
+import android.app.Application
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import de.uniks.codecamp.group_a.weather.data.source.remote.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideWeatherApi(): ApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFusedLocationProviderClient(app: Application): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(app)
+    }
+}
---
+++ app/src/main/java/de/uniks/codecamp/group_a/weather/injection/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package de.uniks.codecamp.group_a.weather.injection
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import de.uniks.codecamp.group_a.weather.data.repository.WeatherRepositoryImpl
+import de.uniks.codecamp.group_a.weather.domain.repository.WeatherRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
