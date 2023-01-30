da06e87c2914e28a3bd7436de487ad30ab806b98
tauberwieblinder <chrisjroeder252@hotmail.de>
2023-01-30 15:32:13 +0100

    fix: reflect file structure in packages and pass sensorViewModel as an argument instead of initializing it in a Composable

--- app/src/main/java/de/uniks/codecamp/group_a/weather/MainActivity.kt
+++ app/src/main/java/de/uniks/codecamp/group_a/weather/MainActivity.kt
@@ -11,15 +11,16 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import de.uniks.codecamp.group_a.weather.ui.screens.EnvironmentSensorScreen
-import de.uniks.codecamp.group_a.weather.ui.screens.WeatherScreen
+import de.uniks.codecamp.group_a.weather.sensor.SensorViewModel
+import de.uniks.codecamp.group_a.weather.ui.screens.weather.WeatherScreen
+import de.uniks.codecamp.group_a.weather.ui.screens.sensor.EnvironmentSensorScreen
 import de.uniks.codecamp.group_a.weather.ui.theme.WeatherTheme
 import de.uniks.codecamp.group_a.weather.viewmodel.WeatherViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val weatherViewModel: WeatherViewModel by viewModels()
-
+    private val sensorViewModel: SensorViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -49,7 +50,7 @@ class MainActivity : ComponentActivity() {
                 )
             }
             composable("sensorScreen") {
-                EnvironmentSensorScreen()
+                EnvironmentSensorScreen(sensorViewModel = sensorViewModel)
             }
         }
     }
