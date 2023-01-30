0d25abd0505d186950a9b005c0f4e01de72789d6
tauberwieblinder <chrisjroeder252@hotmail.de>
2023-01-30 13:46:42 +0100

    feat: make WeatherData compatible after merge conflict

--- app/src/main/java/de/uniks/codecamp/group_a/weather/model/WeatherData.kt
+++ app/src/main/java/de/uniks/codecamp/group_a/weather/model/WeatherData.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "weather")
 data class WeatherData(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val date: String,
     val time: String,
     val temperature: Int,
--- app/src/main/java/de/uniks/codecamp/group_a/weather/model/source/remote/parsing/WeatherDescription.kt
+++ app/src/main/java/de/uniks/codecamp/group_a/weather/model/source/remote/parsing/WeatherDescription.kt
@@ -2,4 +2,5 @@ package de.uniks.codecamp.group_a.weather.model.source.remote.parsing
 
 data class WeatherDescription(
     val description: String,
+    val icon: String
 )
